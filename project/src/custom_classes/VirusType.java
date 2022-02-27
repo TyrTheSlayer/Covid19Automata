ce7ed67cc022e12428f6afcdf1f48de4437848ec
jscarsten <jscarsten0@gmail.com>
2022-02-27 13:33:51 -0700

    Added SimSettings Class to store user setting configuration with tentative default values.

--- project/src/custom_classes/VirusType.java
+++ project/src/custom_classes/VirusType.java
@@ -1,5 +1,6 @@
 /**
  * @author Wesley Camphouse
+ * @author Jonathan Carsten
  *
  * The object representing a type of virus
  */
@@ -29,8 +30,7 @@ public class VirusType {
      * @param minDeathTime The minimum time it takes to die from the virus
      * @param maxDeathTime The maximum time it takes to die from the virus
      */
-    public VirusType(int minContagiousTime, int maxContagiousTime, int minSymptomaticTime, int maxSymptomaticTime,
-                     int minRecoveryTime, int maxRecoveryTime, int minDeathTime, int maxDeathTime) {
+    public VirusType() {
         this.minContagiousTime = minContagiousTime;
         this.maxContagiousTime = maxContagiousTime;
         this.minSymptomaticTime = minSymptomaticTime;
@@ -50,5 +50,69 @@ public class VirusType {
      */
     public Virus genVirus(Person person) {
         return null;
+    }
+
+    public int getMinContagiousTime() {
+        return minContagiousTime;
+    }
+
+    public void setMinContagiousTime(int minContagiousTime) {
+        this.minContagiousTime = minContagiousTime;
+    }
+
+    public int getMaxContagiousTime() {
+        return maxContagiousTime;
+    }
+
+    public void setMaxContagiousTime(int maxContagiousTime) {
+        this.maxContagiousTime = maxContagiousTime;
+    }
+
+    public int getMinSymptomaticTime() {
+        return minSymptomaticTime;
+    }
+
+    public void setMinSymptomaticTime(int minSymptomaticTime) {
+        this.minSymptomaticTime = minSymptomaticTime;
+    }
+
+    public int getMaxSymptomaticTime() {
+        return maxSymptomaticTime;
+    }
+
+    public void setMaxSymptomaticTime(int maxSymptomaticTime) {
+        this.maxSymptomaticTime = maxSymptomaticTime;
+    }
+
+    public int getMinRecoveryTime() {
+        return minRecoveryTime;
+    }
+
+    public void setMinRecoveryTime(int minRecoveryTime) {
+        this.minRecoveryTime = minRecoveryTime;
+    }
+
+    public int getMaxRecoveryTime() {
+        return maxRecoveryTime;
+    }
+
+    public void setMaxRecoveryTime(int maxRecoveryTime) {
+        this.maxRecoveryTime = maxRecoveryTime;
+    }
+
+    public int getMinDeathTime() {
+        return minDeathTime;
+    }
+
+    public void setMinDeathTime(int minDeathTime) {
+        this.minDeathTime = minDeathTime;
+    }
+
+    public int getMaxDeathTime() {
+        return maxDeathTime;
+    }
+
+    public void setMaxDeathTime(int maxDeathTime) {
+        this.maxDeathTime = maxDeathTime;
     }
 }
