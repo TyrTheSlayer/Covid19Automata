bf65d42f6b60eb2c0fb38a8655d3984496c5863f
tyr <aedan.wells@student.nmt.edu>
2022-03-01 15:44:44 -0700

    adding age and real factor-person addition

--- project/src/custom_classes/Factor.java
+++ project/src/custom_classes/Factor.java
@@ -1,5 +1,5 @@
 /**
- * @author Wesley Camphouse
+ * @author Wesley Camphouse, Aedan Wells
  *
  * The class that represents a single factor influencing infection
  */
@@ -9,6 +9,9 @@ package custom_classes;
 public class Factor {
     private double severityGet;
     private double severityGive;
+    private double age;
+
+
 
     //Constructors
     /**
@@ -17,9 +20,10 @@ public class Factor {
      * @param severityGet How much this factor affects inward infection chance
      * @param severityGive How much this factor affects outward infection chance
      */
-    public Factor(double severityGet, double severityGive) {
+    public Factor(double severityGet, double severityGive, double age) {
         this.severityGet = severityGet;
         this.severityGive = severityGive;
+        this.age = age;
     }
 
     //Methods
@@ -42,4 +46,14 @@ public class Factor {
     public double applyFactorGive(double oldChance) {
         return oldChance * this.severityGive;
     }
+
+    /**
+     * gets age
+     *
+     * @returns age of person
+     */
+    public double getAge(){
+        return this.age;
+    }
+
 }
