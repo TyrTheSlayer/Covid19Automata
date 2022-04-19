e7214a8cde39100f9980d37b61b6fae9d9ec9a64
tyr <aedan.wells@student.nmt.edu>
2022-04-18 19:00:14 -0600

    Minor postsim updates

--- project/src/UI/PostSimUI.java
+++ project/src/UI/PostSimUI.java
@@ -6,19 +6,35 @@
 
 package UI;
 
-import Simulator.SimSettings;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class PostSimUI extends JFrame {
-
+    public Scanner sc;
+    public String[] finalDayData;
     public PostSimUI(String title) {
         super(title);
         setLocationRelativeTo(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        String data = "";
+        try{
+            this.sc = new Scanner(new File("./postsim/simulation.csv"));
+            sc.useDelimiter(",");
+            while(sc.hasNext()){
+                data = sc.next();
+            }
+            this.finalDayData = data.split(",");
+
+        } catch  (Exception e) {
+            e.printStackTrace();
+        }
 
         this.addWindowListener(new WindowAdapter() {
             @Override
@@ -28,33 +44,30 @@ public class PostSimUI extends JFrame {
         });
     }
 
-    public static void startWindow(){
-        PostSimUI frame = new PostSimUI("Test");
-
+    /**
+     * Starts the Post Sim UI window
+     */
+    public void startWindow(){
         try {
-            // UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        JLabel results = new JLabel("Results");
-        results.setBounds(493, 120, 110, 30);
-        results.setFont(new Font("Verdana", Font.PLAIN, 12));
+        JLabel results = new JLabel("Final Results:");
+        results.setBounds(20, 10, 400, 30);
+        results.setFont(new Font("Verdana", Font.PLAIN, 24));
+        this.add(results);
 
-        frame.add(results);
-
+        JLabel succeptible = new JLabel("Succeptible: ");
+        succeptible.setBounds(20, 40, 200, 100);
+        results.setFont(new Font("Verdana", Font.PLAIN, 18));
+        this.add(succeptible);
         //Set size
-        frame.setSize(700, 370) ;
-
+        this.setSize(700, 700);
         // uses no layout managers
-        frame.setLayout(null);
-
+        this.setLayout(null);
         // makes the frame visible
-        frame.setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        startWindow();
+        this.setVisible(true);
     }
 }
