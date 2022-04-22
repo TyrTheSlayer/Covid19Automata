8fe0f2f54b04bbf9e7c5da910ecf189d0234ae80
tyr <aedan.wells@student.nmt.edu>
2022-04-21 21:04:33 -0600

    Basic postsim updates

--- project/src/UI/PostSimUI.java
+++ project/src/UI/PostSimUI.java
@@ -26,10 +26,12 @@ public class PostSimUI extends JFrame {
         String data = "";
         try{
             this.sc = new Scanner(new File("./postsim/simulation.csv"));
-            sc.useDelimiter(",");
+            sc.useDelimiter("\n");
             while(sc.hasNext()){
+                System.out.println(sc);
                 data = sc.next();
             }
+            this.finalDayData = new String[5];
             this.finalDayData = data.split(",");
 
         } catch  (Exception e) {
@@ -59,10 +61,30 @@ public class PostSimUI extends JFrame {
         results.setFont(new Font("Verdana", Font.PLAIN, 24));
         this.add(results);
 
-        JLabel succeptible = new JLabel("Succeptible: ");
+        JLabel succeptible = new JLabel("Susceptible: " + finalDayData[0]);
         succeptible.setBounds(20, 40, 200, 100);
         results.setFont(new Font("Verdana", Font.PLAIN, 18));
         this.add(succeptible);
+
+        JLabel infected = new JLabel("Infected: " + finalDayData[1]);
+        infected.setBounds(20, 70, 200, 100);
+        results.setFont(new Font("Verdana", Font.PLAIN, 18));
+        this.add(infected);
+
+        JLabel recovered = new JLabel("Recovered: " + finalDayData[2]);
+        recovered.setBounds(20, 100, 200, 100);
+        results.setFont(new Font("Verdana", Font.PLAIN, 18));
+        this.add(recovered);
+
+        JLabel dead = new JLabel("Dead: " + finalDayData[3]);
+        dead.setBounds(20, 130, 200, 100);
+        results.setFont(new Font("Verdana", Font.PLAIN, 18));
+        this.add(dead);
+
+        JLabel vaxx = new JLabel("Vaccination: " + finalDayData[4]);
+        vaxx.setBounds(20, 160, 200, 100);
+        results.setFont(new Font("Verdana", Font.PLAIN, 18));
+        this.add(vaxx);
         //Set size
         this.setSize(700, 700);
         // uses no layout managers
