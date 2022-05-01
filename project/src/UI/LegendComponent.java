e8e1a7f958cc3e711e4309e0b681d55f0fa223ac
snix <samuel.nix@student.nmt.edu>
2022-04-30 23:11:13 -0600

    push

--- project/src/UI/LegendComponent.java
+++ project/src/UI/LegendComponent.java
@@ -25,48 +25,48 @@ public class LegendComponent extends JComponent {
         JLabel leg = new JLabel("Legend:");
         leg.setForeground(Color.white);
         leg.setBackground(Color.white);
-        leg.setBounds(50, 50, 200, 20);
+        leg.setBounds(50, 50, 200, 25);
         leg.setFont(f);
         add(leg);
 
         //JLabels for Infection Status
         JLabel sus = new JLabel("Susceptible:");
         sus.setForeground(Color.white);
-        sus.setBounds(50,100,200,20);
+        sus.setBounds(50,100,200,25);
         sus.setFont(f);
         add(sus);
 
         JLabel inf = new JLabel("Infected:");
         inf.setForeground(Color.white);
-        inf.setBounds(50,130,200,20);
+        inf.setBounds(50,130,200,25);
         inf.setFont(f);
         add(inf);
 
         JLabel rec = new JLabel("Recovered:");
         rec.setForeground(Color.white);
-        rec.setBounds(50,160,200,20);
+        rec.setBounds(50,160,200,25);
         rec.setFont(f);
         add(rec);
 
         g.setColor(Color.decode("#0072B2"));
-        g.fillOval(200,100,25,25);
+        g.fillOval(200,100,20,20);
 
         g.setColor(Color.decode("#CC79A7"));
-        g.fillOval(200,130,25,25);
+        g.fillOval(200,130,20,20);
 
         g.setColor(Color.CYAN);
-        g.fillOval(200,160,25,25);
+        g.fillOval(200,160,20,20);
 
         //JLabels for Entrance/Exits
         JLabel ent = new JLabel("Entrance:");
         ent.setForeground(Color.white);
-        ent.setBounds(50,210,200,20);
+        ent.setBounds(50,210,200,25);
         ent.setFont(f);
         add(ent);
 
         JLabel exit = new JLabel("Exit:");
         exit.setForeground(Color.white);
-        exit.setBounds(50,240,200,20);
+        exit.setBounds(50,240,200,25);
         exit.setFont(f);
         add(exit);
 
